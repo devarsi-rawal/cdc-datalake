6c7b1fd0a78ebfed4ecd50089a61b6edfda94c27
KenedyT <kenedyt15@gmail.com>
2020-04-18 12:31:43 -0400

    added Ethnicity & Race filters

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ import math
     
 def filter_update(attrname, old, new):
     filter_df = orig_pddf[orig_pddf["age"].between(age_slider.value[0], age_slider.value[1])]
+    filter_df = filter_df[filter_df['race'].isin(race_multi_select.value)]
+    filter_df = filter_df[filter_df['ethnicity'].isin(ethnicity_multi_select.value)]
     filter_df = filter_df[filter_df['disease'].isin(multi_select.value)]
     filter_df = filter_df.groupby(['disease']).sum()
     new_source = ColumnDataSource(filter_df)
@@ -63,6 +65,23 @@ ages.sort()
 age_slider = RangeSlider(title="Age", start=ages[0], end=ages[-1], value=(ages[0], ages[-1]), step=5, sizing_mode="stretch_width")
 age_slider.on_change("value", filter_update)
 
+#race multiselect
+races.sort()
+races = list(map(str, races))
+plot_races = figure(plot_height=700, plot_width=1000, x_range=races, sizing_mode="stretch_both")
+
+race_multi_select = MultiSelect(title="Races", value=races, options=races, height=200, sizing_mode="stretch_width")
+race_multi_select.on_change('value', filter_update)
+
+#ethnicity muleiselect
+ethns.sort()
+ethns = list(map(str, ethns))
+plot_ethns = figure(plot_height=700, plot_width=1000, x_range=ethns, sizing_mode="stretch_both")
+
+
+ethnicity_multi_select = MultiSelect(title="Ethnicity", value=ethns, options=ethns, height=200, sizing_mode="stretch_width")
+ethnicity_multi_select.on_change('value', filter_update)
+
 # Add plot details
 plot.vbar(x='disease', top="case counts", source=source, width=0.70)
 plot.title.text = "Case Counts per Disease"
@@ -79,7 +98,7 @@ hover.tooltips = [
 
 hover.mode = "vline"
 plot.add_tools(hover)
-filters = column([multi_select, age_slider], width=500, height=1000)
+filters = column([multi_select, age_slider, ethnicity_multi_select, race_multi_select], width=500, height=1000)
 bokeh_doc.add_root(row([filters, plot]))
 
 bokeh_doc.title = "CDC Data Lake"
